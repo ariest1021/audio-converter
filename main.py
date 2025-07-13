e5c30ed72e8882e8d2116b74381418cc6cf31f94
LuminaryProject <atabor1021@gmail.com>
2025-07-12 21:15:30 -0700

    guh

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ outputAudio = "audioOutput"
 audioFormat = "ogg" # Change to whatever format you want the audio file to be converted to.
 
 
-vext = (".wav", ".flac", ".m4a", ".aac", ".mp3")
-# Loop that goes through each file and converts to whatever format
-for filename in os.listdir(inputAudio):
+vext = (".wav", ".flac", ".m4a", ".aac", ".mp3") # valid formats
+
+for filename in os.listdir(inputAudio): # Loop that goes through each file and converts to whatever format
     if filename.endswith(vext):
         try:
             (
                 ffmpeg
                 .input(os.path.join(inputAudio, filename))
-                .output(os.path.join(outputAudio, os.path.splitext(filename)[0] + "." + audioFormat))
+                .output(os.path.join(outputAudio, os.path.splitext(filename)[0] + "." + audioFormat),
+                codec='libopus',
+                audio_bitrate='128k',
+                ac=2,
+                ar=48000,
+                threads=0
+                )
                 .run()
             )
         except ffmpeg.Error as e:
