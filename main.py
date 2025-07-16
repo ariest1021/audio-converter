769ac1c61bba965eee5e6f197d563ebb1096271e
LuminaryProject <atabor1021@gmail.com>
2025-07-15 19:16:31 -0700

    Refactoring, explanatory comments, file filtering
    Added explanatory comments to variables and ffmpeg parameters for better readability. Updated file filtering to exclude files already in the target format before processing, preventing unnecessary conversions.

--- main.py
+++ main.py
@@ -4,37 +4,42 @@ import os
 from tqdm import tqdm
 from multiprocessing import Pool
 
-inputAudio = "audioInput"
-outputAudio = "audioOutput"
+inputAudio = "audioInput" # Input Folder
+outputAudio = "audioOutput" # Output Folder
 audioFormat = "ogg" # Change to whatever format you want the audio file to be converted to.
-
-
 vext = (".wav", ".flac", ".m4a", ".aac", ".mp3", ".ogg") # valid formats
-files = [f for f in os.listdir(inputAudio) if f.lower().endswith(vext)]
-def convertAudio(filename):
 
+
+
+
+def convertAudio(filename):
     if filename.lower().endswith("." + audioFormat):
-           return
+        return
     try:
                 (
                 ffmpeg
                 .input(os.path.join(inputAudio, filename))
                 .output(os.path.join(outputAudio, os.path.splitext(filename)[0] + "." + audioFormat),
-                codec='libopus',
-                audio_bitrate='128k',
-                ac=2,
-                ar=48000,
-                threads=1
+                codec='libopus', # Encoder
+                audio_bitrate='128k', # Audio Bitrate 
+                ac=2, # Audio channels
+                ar=48000, # Audio sample rate
+                threads=1 
                 )
                 .run(quiet=True, overwrite_output=True)
             )
     except ffmpeg.Error as e:
-                return
+        return
+
+
+
 
 if __name__ == "__main__":
     os.makedirs(outputAudio, exist_ok=True)
-    files = [f for f in os.listdir(inputAudio) if f.lower().endswith(vext)]
-
+    files = [
+    f for f in os.listdir(inputAudio)
+    if f.lower().endswith(vext) and not f.lower().endswith("." + audioFormat)
+]
     with Pool(processes=os.cpu_count()) as pool:
         for result in tqdm(pool.imap_unordered(convertAudio, files), total=len(files), desc="Converting"):
             if result:
